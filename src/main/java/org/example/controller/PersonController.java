0b1ebcf0a5b7fed8f359ba637a091cbefb910eb9
Vasil Pushkarov <pu6karev@gmail.com>
2024-08-06 00:25:06 +0300

    checking the interaction of two entities

--- src/main/java/org/example/controller/PersonController.java
+++ src/main/java/org/example/controller/PersonController.java
@@ -27,48 +27,50 @@ public class PersonController {
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model){
         model.addAttribute("person", personDAO.show(id));
+        model.addAttribute("books", personDAO.getBooksByPersonId(id));
+
         return "people/show";
     }
 
-//    @GetMapping("/new")
-//    public String newPerson(Model model){
-//        model.addAttribute("person", new Person());
-//
-//        return "people/new";
-//    }
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person", new Person());
 
-//    @PostMapping()
-//    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
-//
-//        if(bindingResult.hasErrors()){
-//            return "people/new";
-//        }
-//
-//        personDAO.save(person);
-//        return "redirect:/people";
-//    }
-//
-//    @GetMapping("/{id}/edit")
-//    public String edit(Model model, @PathVariable("id") int id){
-//        model.addAttribute("person", personDAO.show(id));
-//
-//        return "people/edit";
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
-//                         @PathVariable("id") int id){
-//        if(bindingResult.hasErrors()){
-//            return "people/edit";
-//        }
-//
-//        personDAO.update(id, person);
-//        return "redirect:/people";
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public String delete(@PathVariable("id") int id){
-//        personDAO.delete(id);
-//        return "redirect:/people";
-//    }
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "people/new";
+        }
+
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id){
+        model.addAttribute("person", personDAO.show(id));
+
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id){
+        if(bindingResult.hasErrors()){
+            return "people/edit";
+        }
+
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id){
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
 }
--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,13 +1,23 @@
 package org.example.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 public class Person {
     private int id;
 
+    @NotEmpty(message = "First Name should not be empty")
     private String firstName;
+    @NotEmpty(message = "Last Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 symbols")
     private String lastName;
 
+    @Past(message = "Birth date should be in the past")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate birthDate;
 
     public Person(){}
